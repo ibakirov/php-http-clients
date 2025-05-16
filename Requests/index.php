9415c00758783ea590b2ba2e6a227512d2fd3d30
Ilyas Bakirov <i.bakirov@gmail.com>
2025-05-16 20:50:34 +0500

    Upgrade library rmccue/requests

--- Requests/index.php
+++ Requests/index.php
@@ -2,23 +2,48 @@
 
 require_once __DIR__. '/../vendor/autoload.php';
 
-$URL = 'https://jsonplaceholder.typicode.com/todos';
+use WpOrg\Requests\Requests;
+
+WpOrg\Requests\Autoload::register();
+
+$URL = 'https://reqres.in/api/users?page=2';
 
 $headers = array('Accept' => 'application/json');
 $options = array();
 
 $start_time = microtime(true);
-$request = Requests::get($URL, $headers, $options);
+
+$response = Requests::get($URL, $headers, $options);
+
 $time_elapsed = microtime(true) - $start_time;
 
 // Print request execution time
 echo "Request took: " . $time_elapsed . " seconds." . PHP_EOL;
 
-var_dump($request);
+// Get response status code
+echo "Response Status: " . $response->status_code . PHP_EOL;
 
-// int(200)
-echo $request->status_code . PHP_EOL;
-// string(31) "application/json; charset=utf-8"
-echo $request->headers['content-type'] . PHP_EOL;
+// Get response headers
+$headers = $response->headers;
 
-echo $request->body;
+echo "Response Headers: " . PHP_EOL;
+
+foreach ($headers as $key => $value) {
+    if (is_array($value)) {
+        $value = implode(", ", $value);
+    }
+    echo "$key: $value" . PHP_EOL;
+}
+
+// Get response body
+$data = $response->body;
+
+echo "Response Body: " . $data . PHP_EOL;
+
+// Manipulate data
+$users = json_decode($data, true);
+
+// Print user data
+echo "User Data: " . PHP_EOL;
+
+print_r($users);
